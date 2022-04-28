41c6f6904fe45ffbf2d3746e508877ec8468f0a1
Lamor Odingo <l2odingo@gmail.com>
2022-04-27 19:11:35 -0700

    added userRoutes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,2 +1,17 @@
 const router = require('express').Router();
+// require all methods from userController.ks and put inside a const object
+const {
+    getUsers,
+    getSingleUser,
+    createNewUser,
+    updateUser,
+    deleteUser,
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getUsers).post(createNewUser);
+
+// /api/users/userId
+router.route('/:userId').get(getSingleUser).delete(deleteUser).update(updateUser);
+
 
